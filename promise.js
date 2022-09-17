6c3fe1dbf6f0866ddd3d2d6f0ec1ec3de431c904
aytidasaputra <aytidasaputra@gmail.com>
2022-09-17 09:23:37 +0700

    update promise output

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+let promiseOutput = async (value) => {
+  let dataIXX = await promiseTheaterIXX()
+  let dataVGC = await promiseTheaterVGC()
+  var dataResult = [].concat(dataIXX, dataVGC);
+  let count = 0;
+
+  dataResult.forEach((v) => (v.hasil === value && count++));
+  return count;
+}
 
 module.exports = {
   promiseOutput,
